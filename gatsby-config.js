e4e64196deaf318270d1eb07c0f4034e39b6c369
A Groshek <amy@amygroshek.com>
2020-02-26 16:51:38 -0600

    Removes gatsby-plugin-react-leaflet

--- gatsby-config.js
+++ gatsby-config.js
@@ -147,13 +147,7 @@ module.exports = {
           }
         })
       }
-    },
-    {
-      resolve: 'gatsby-plugin-react-leaflet',
-      options: {
-        linkStyles: true // (default: true) Enable/disable loading stylesheets via CDN
-      }
-    },
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
