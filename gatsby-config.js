8958a4a9e625a9a67f10845c7dbc7d697c68d043
A Groshek <amy@amygroshek.com>
2020-02-26 11:40:52 -0600

    Update favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,7 +66,7 @@ module.exports = {
         background_color: `#fff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/ms-icon.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-sass`,
